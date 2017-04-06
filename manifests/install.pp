e2d3be656cc8c61a9c228d22cb8babc9ccf6614c
Chris Drzewiecki <chris@fdisk.io>
2017-04-06 13:31:59 -0600

    Initial pass on install class

---
+++ manifests/install.pp
@@ -0,0 +1,10 @@
+# @api private
+# This class installs chrony. It should not be called on its own.
+class chrony::install inherits chrony {
+  if $chrony::package_manage == true {
+    package { 'chrony':
+      ensure => $chrony::package_ensure,
+      name => $chrony::package_name,
+    }
+  }
+}
